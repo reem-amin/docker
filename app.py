ec9a580bbf94dde9e371dd58a02ab3aeccf30bdb
root <root@DESKTOP-7UAVP4E.localdomain>
2025-10-06 13:44:06 +0300

    First commit - My Docker MySQL app

---
+++ app.py
@@ -0,0 +1,16 @@
+import mysql.connector
+import time
+
+while True:
+    try:
+        db = mysql.connector.connect(
+            host="db",
+            user="root",
+            password="db_pass123",
+        )
+        print("✅ Connected to MySQL successfully!")
+        break
+    except mysql.connector.Error as err:
+        print("❌ Connection failed, retrying in 3s:", err)
+        time.sleep(3)
+
